98ed8c455f5201c92c6ad645fda6fb83a6d4bc6e
Matthias <xmatthias@outlook.com>
2020-02-16 17:51:52 +0100

    Use . instead of -

--- freqtrade/__init__.py
+++ freqtrade/__init__.py
@@ -6,7 +6,7 @@ if __version__ == 'develop':
     try:
         import subprocess
         prefix = subprocess.check_output(['git', 'tag']).decode('utf-8').split()[-1]
-        __version__ = prefix + '-dev-' + subprocess.check_output(
+        __version__ = prefix + '.dev-' + subprocess.check_output(
             ['git', 'log', '--format="%h"', '-n 1'],
             stderr=subprocess.DEVNULL).decode("utf-8").rstrip().strip('"')
     except Exception:
